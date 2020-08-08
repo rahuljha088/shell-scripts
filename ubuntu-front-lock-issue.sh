c1481accb5b4046516d5c402faa1c21b8c71d12c
Rahul <2478728+rahuljha088@users.noreply.github.com>
2020-08-08 13:28:22 +0800

    Add files via upload
    script to get rid of front lock related issue in ubuntu during apt-get install

---
+++ ubuntu-front-lock-issue.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo killall apt apt-get
+
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock*
+
+sudo dpkg --configure -a
+
+sudo apt update
